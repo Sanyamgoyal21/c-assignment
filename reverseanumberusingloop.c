b6154f3ce0ff8011afa598466d9b02f4a5f1cd98
Sanyamgoyal21 <146330589+Sanyamgoyal21@users.noreply.github.com>
2023-10-02 17:28:49 +0530

    Update reverseanumberusingloop.c

--- reverseanumberusingloop.c
+++ reverseanumberusingloop.c
@@ -5,8 +5,9 @@ int main()
     int n, rem, reverse;
     printf("enter n");
     scanf("%d", &n);
-    for (reverse = 0; rem = n % 10; n = n / 10)
+    for (reverse = 0;n>0 ; n = n / 10)
     {
+        rem = n % 10
         reverse = reverse * 10 + rem;
     }
     printf("%d", reverse);
